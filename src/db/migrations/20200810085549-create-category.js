d9350f290876e5550af47513b2b611fcd70af785
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-10 11:56:38 +0300

    Defined categories migration

---
+++ src/db/migrations/20200810085549-create-category.js
@@ -0,0 +1,32 @@
+export default {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Categories');
+  },
+};
